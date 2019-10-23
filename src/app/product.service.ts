d6c610e34bc9490db75eace30c649682dd5e6dde
theonlyamos <theonlyamos@gmail.com>
2019-10-23 14:52:46 +0000

    passed all sixth tests

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,15 +5,21 @@ import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
+  private _productsUrl: string = "../assets/products.json";
 
   constructor(private _http: Http) { }
 
-  public getAlbum(id: number) : Observable<Album> {
+  getAlbum(id: number) : Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json())
+  }
+
+  getProducts() : Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json())
   }
 
 }
