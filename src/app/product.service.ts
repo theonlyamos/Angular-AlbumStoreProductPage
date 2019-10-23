236b10bcbe0c6c3c7795bd845e5754dbc24535c5
theonlyamos <theonlyamos@gmail.com>
2019-10-23 14:03:38 +0000

    passed all fourth test

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -9,8 +12,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  public getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json())
+  public getAlbum(id: number) : Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
   }
 
 }
